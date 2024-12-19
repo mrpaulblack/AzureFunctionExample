6865a4a5cb1979ef9b478edbb5c7541d1e90e2d4
Paul Braeuning <paul@paulgo.io>
2024-12-19 15:07:09 +0100

    feat: add list book and get book endpoint

--- Function/Books.cs
+++ Function/Books.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using System.Net;
+using AzureFunctionExample.Model;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
@@ -8,17 +10,34 @@ using Microsoft.OpenApi.Models;
 
 namespace AzureFunctionExample;
 
-public class Books(ILoggerFactory loggerFactory)
+public class Books
 {
-    private readonly ILogger _logger = loggerFactory.CreateLogger<Books>();
+    private readonly ILogger _logger;
+    private readonly IList<BookModel> _books;
 
-    // function called Books
+    public Books(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<Books>();
+
+        // define list of books
+        _books =
+        [
+            new(
+                Author: "Marie Musterfrau",
+                Title: "Computer Science 101",
+                PublishYear: 2019,
+                Genre: Enum.Genre.Scientific
+            ),
+            new("Max Musterman", "Cyperpunk", 1994, Enum.Genre.Fiction),
+        ];
+    }
+
+    // get list of all books
     [OpenApiOperation(
-        operationId: "books",
+        operationId: "listBooks",
         tags: ["books"],
         Summary = "List Books",
-        Description = "Get list of books.",
-        Visibility = OpenApiVisibilityType.Important
+        Description = "Get list of books."
     )]
     [OpenApiSecurity(
         "function_key",
@@ -33,19 +52,80 @@ public class Books(ILoggerFactory loggerFactory)
         Summary = "The response",
         Description = "A list of books in json format."
     )]
-    [Function("Books")]
-    public HttpResponseData Run(
-        [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData request
+    [Function("HTTPListBooks")]
+    public async Task<HttpResponseData> ListBooks(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "book")] HttpRequestData request
     )
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        _logger.LogInformation(
+            $"[{request.FunctionContext.InvocationId}] Processing request for list books endpoint."
+        );
 
         var response = request.CreateResponse(HttpStatusCode.OK);
 
-        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-        response.WriteString("Welcome to Azure Functions!");
+        await response.WriteAsJsonAsync(_books);
 
         return response;
+    }
+
+    // get specific book by its title
+    [OpenApiOperation(
+        operationId: "getBook",
+        tags: ["books"],
+        Summary = "Get Book",
+        Description = "Get a book by its title."
+    )]
+    [OpenApiSecurity(
+        "function_key",
+        SecuritySchemeType.ApiKey,
+        Name = "code",
+        In = OpenApiSecurityLocationType.Query
+    )]
+    [OpenApiParameter(
+        name: "bookTitle",
+        In = ParameterLocation.Path,
+        Required = true,
+        Type = typeof(string),
+        Summary = "Title of book to return",
+        Description = "Return the book for the given title."
+    )]
+    [OpenApiResponseWithBody(
+        statusCode: HttpStatusCode.OK,
+        contentType: "application/json",
+        bodyType: typeof(string),
+        Summary = "The response",
+        Description = "A book in json formet."
+    )]
+    [OpenApiResponseWithoutBody(
+        statusCode: HttpStatusCode.NotFound,
+        Summary = "Book not found",
+        Description = "Returned when there is no book found for the given title."
+    )]
+    [Function("HTTPGetBook")]
+    public async Task<HttpResponseData> GetBook(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "book/{bookTitle}")]
+            HttpRequestData request,
+        string bookTitle
+    )
+    {
+        _logger.LogInformation(
+            $"[{request.FunctionContext.InvocationId}] Processing request to get specific book by its title with the title {bookTitle}."
+        );
+
+        try
+        {
+            var selectedBook = _books.Single(x => x.Title == bookTitle);
+
+            var response = request.CreateResponse(HttpStatusCode.OK);
+            await response.WriteAsJsonAsync(selectedBook);
+            return response;
+        }
+        catch (InvalidOperationException)
+        {
+            _logger.LogWarning(
+                $"[{request.FunctionContext.InvocationId}] Error: {bookTitle} was not found."
+            );
+            return request.CreateResponse(HttpStatusCode.NotFound);
+        }
     }
 }
